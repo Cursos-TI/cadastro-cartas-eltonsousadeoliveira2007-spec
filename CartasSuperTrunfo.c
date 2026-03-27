ff4e83c8f4a0d199b176a0585dae3cf788b68412
eltonsousadeoliveira2007-spec <eltonsousadeoliveira2007@gmail.com>
2026-03-27 05:55:27 +0000

    Fiz o desafio super trunfo novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,72 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado;
+  char codigo;
+  char cidade;
+  int populacao;
+  float area;
+  float pib;
+  int turismo;
 
   // Área para entrada de dados
+  printf("Digite os dados da carta 01.\n");
+  
+  printf("Qual é o Estado:\n");
+  scanf("%s", &estado);
+  
+  printf("Digite o código da carta:\n");
+  scanf("%s", &codigo);
+
+  printf("Nome da Cidade:\n");
+  scanf("%s", &cidade);
+
+  printf("Qual o número populacional:\n");
+  scanf("%d", &populacao);
+
+  printf("Informe a área territorial em km²:\n");
+  scanf("%f", &area);
+
+  printf("Valor do PIB:\n");
+  scanf("%f", &pib);
+
+  printf("Quantidade de pontos turísticos:\n");
+  scanf("%d", &turismo);
+  
+  printf("Os dados informados pelo usuário para a carta 01 foram:\n");
+  
+  printf("Estado: %s - Código: %s - Nome da Cidade: %s - População: %d", estado, codigo, cidade, populacao);
+  printf("Àrea: %f - PIB: %f - Números de Pontos Turísticos: %d", area, pib, turismo);
+
+printf("Digite os dados da carta 02.\n");
+  
+  printf("Qual é o Estado:\n");
+  scanf("%s", &estado);
+  
+  printf("Digite o código da carta:\n");
+  scanf("%s", &codigo);
+
+  printf("Nome da Cidade:\n");
+  scanf("%s", &cidade);
+
+  printf("Qual o número populacional:\n");
+  scanf("%d", &populacao);
+
+  printf("Informe a área territorial em km²:\n");
+  scanf("%f", &area);
+
+  printf("Valor do PIB:\n");
+  scanf("%f", &pib);
+
+  printf("Quantidade de pontos turísticos:\n");
+  scanf("%d", &turismo);
+  
+  printf("Os dados informados pelo usuário para a carta 02 foram:\n");
+  
+  printf("Estado: %s - Código: %s - Nome da Cidade: %s - População: %d", estado, codigo, cidade, populacao);
+  printf("Àrea: %f - PIB: %f - Números de Pontos Turísticos: %d", area, pib, turismo);
+
+  
 
   // Área para exibição dos dados da cidade
 
